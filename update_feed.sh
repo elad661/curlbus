43c9ede93896bf69e421e23284a9be397782b291
Elad Alfassa <ealfassa@redhat.com>
2018-06-03 14:33:51 +0300

    Workaround bad CSVs in the GTFS feed
    The CSVs sometimes use " in string fields with no escaping.
    This confuses psql if there's an even number of " characters,
    because it things these are quotes.
    
    I tried using sed to append \ before them, but that didn't help,
    so I decided to cheat and tell psql that the quote character is
    U+0007 (BELL) which will probably never appear in the CSV file.

--- update_feed.sh
+++ update_feed.sh
@@ -6,10 +6,13 @@ python3 load_translations.py israel-public-transportation.zip
 TEMP_DIR="$(mktemp -d)"
 unzip israel-public-transportation.zip -d "$TEMP_DIR"
 pushd "$TEMP_DIR"
+# Hack! use  (U+0007, bell) as a quote character, since this will probably
+# will never appear in the CSV. This will avoid skipping lines when "
+# is used in the original CSV
 psql -d gtfs -c "\\copy agency from 'agency.txt' with csv header"
-psql -d gtfs -c "\\copy stops from 'stops.txt' with csv header"
-psql -d gtfs -c "\\copy routes from 'routes.txt' with csv header"
-psql -d gtfs -c "\\copy trips from 'trips.txt' with csv header"
+psql -d gtfs -c "\\copy stops from stops.txt with csv header quote ''"
+psql -d gtfs -c "\\copy routes from routes.txt with csv header quote ''"
+psql -d gtfs -c "\\copy trips from trips.txt with csv header quote ''"
 psql -d gtfs -c "\\copy stoptimes from 'stop_times.txt' with csv header"
 popd
 rm -fr "$TEMP_DIR"
