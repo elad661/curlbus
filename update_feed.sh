72308dc75c7216d116a20c31796698ff86199fbb
Elad Alfassa <elad@fedoraproject.org>
2018-06-19 20:37:06 +0300

    Remove load_translations.py and move it to update_feed.sh
    load_translations was originally written as a loader for all GTFS
    types in the model, but when I realized how slow it was I looked
    for faster ways, until I settled on using `psql` and `/copy`.
    
    Even after this was all done with a shell script, translations
    were still loaded by the python script because they often contain
    quotes, which makes psql's csv parser sad.
    
    With the custom quote character hack this is no longer an issue,
    and translations can be loaded with psql directly.
    
    This saves *a lot* of time when loading the GTFS feed. On my
    computer (with an SSD) it took 6 minutes to load the feed
    (not counting the download time)

--- update_feed.sh
+++ update_feed.sh
@@ -3,17 +3,23 @@ set -euo pipefail
 POSTGRES_DSN="$(python3 -c 'import configparser; c = configparser.ConfigParser(); c.read("config.ini"); print(c["gino"]["dsn"])')"
 wget ftp://gtfs.mot.gov.il/israel-public-transportation.zip -O israel-public-transportation.zip
 python3 create_tables.py
-python3 load_translations.py israel-public-transportation.zip
 TEMP_DIR="$(mktemp -d)"
 unzip israel-public-transportation.zip -d "$TEMP_DIR"
 pushd "$TEMP_DIR"
 # Hack! use  (U+0007, bell) as a quote character, since this will probably
 # will never appear in the CSV. This will avoid skipping lines when "
 # is used in the original CSV
+echo -n "loading translations: "
+psql $POSTGRES_DSN -c "\\copy translations from 'translations.txt' with csv header quote ''"
+echo -n "loading agencies:     "
 psql $POSTGRES_DSN -c "\\copy agency from 'agency.txt' with csv header"
+echo -n "loading stops:        "
 psql $POSTGRES_DSN -c "\\copy stops from stops.txt with csv header quote ''"
+echo -n "loading routes:       "
 psql $POSTGRES_DSN -c "\\copy routes from routes.txt with csv header quote ''"
+echo -n "loading trips:        "
 psql $POSTGRES_DSN -c "\\copy trips from trips.txt with csv header quote ''"
+echo -n "loading stoptimes:    "
 psql $POSTGRES_DSN -c "\\copy stoptimes from 'stop_times.txt' with csv header"
 popd
 rm -fr "$TEMP_DIR"
